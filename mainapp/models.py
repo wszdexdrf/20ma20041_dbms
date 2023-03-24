b8bc1de461ac2cb50cd8572e7943b16a1ec72a57
wszdexdrf <piyushkumar2k02@kgpian.iitkgp.ac.in>
2023-03-24 12:45:34 +0530

    Update database model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+class Faculty(models.Model):
+    faculty_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=12)
+    department_id = models.ForeignKey("Department", on_delete=models.CASCADE)
+
+
+class Department(models.Model):
+    department_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    dept_head_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+
 class Course(models.Model):
+    course_id = models.BigAutoField(primary_key=True)
     course_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
     credits = models.IntegerField()
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
+    departmend_id = models.OneToOneField(Department)
