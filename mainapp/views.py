77c5f36fddba9c3f4eb56909594759d183fa1a34
wszdexdrf <piyushkumar2k02@kgpian.iitkgp.ac.in>
2023-03-24 11:06:45 +0530

    Rename files. Fix structure

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def home(request):
+    return render(request, "HTML/base.html")
+
+
 def courses(request):
     return render(request, "HTML/courses.html")
